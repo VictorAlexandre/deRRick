3d1468e7fdfd7177e73bc331a50efb316b98669d
Victor Alexandre <victor@victoralexandre.fr>
2014-12-04 10:54:36 +0100

    Update and rename wesanderson.R to deRRick.R

---
+++ R/deRRick.R
@@ -0,0 +1,14 @@
+#' namelist
+#'
+#' A data.frame containing list of Derrick's best colors for which we have palettes.
+#' @docType data
+#' @keywords datasets
+#' @name namelist
+#' @usage data(namelist)
+NULL
+
+#' @title deRRick
+#' @name deRRick
+#' @docType package
+#' @details list of palettes from Inspector Derrick series
+NULL
